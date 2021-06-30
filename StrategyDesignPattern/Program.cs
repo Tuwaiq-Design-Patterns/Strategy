8a89c25f787e00a8033d0b3d9aac8975dcc02579
Abdullah Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-06-30 19:00:37 +0300

    payment strategy

---
+++ StrategyDesignPattern/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace StrategyDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new();
+            context.SetPaymentMethod(new MadaPayment());
+            context.Pay();
+            
+            context.SetPaymentMethod(new VisaPayment());
+            context.Pay();
+            
+            context.SetPaymentMethod(new MasterCardPayment());
+            context.Pay();
+
+        }
+    }
+
+    interface IPaymentMethod
+    {
+        public void Pay();
+    }
+
+    class MadaPayment : IPaymentMethod
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payed Through Mada");
+        }
+    }
+
+    class VisaPayment : IPaymentMethod
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payed Through Visa");
+        }
+    }
+    class MasterCardPayment : IPaymentMethod
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Payed Through Master Card");
+        }
+    }
+
+    class Context
+    {
+        private IPaymentMethod _paymentMethod;
+        public Context(){}
+
+        public Context(IPaymentMethod paymentMethod)
+        {
+            _paymentMethod = paymentMethod;
+        }
+
+
+        public void SetPaymentMethod(IPaymentMethod paymentMethod)
+        {
+            _paymentMethod = paymentMethod;
+        }
+
+        public void Pay()
+        {
+            _paymentMethod.Pay();
+        }
+    }
+    
+}
