b541256da1bf65aba50d4c0feb6c84e4f4fbbdf9
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-06-30 20:19:31 +0300

    Strategy IMPLMENTATION

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+
+
+<div dir="ltr" align="center">
+
+![Tuwaiq](https://i.ibb.co/SV2BSn5/tuwaiq.png)
+
+</div>
+
+<hr />
+
 # Strategy
 this repo contains strategy design pattern implementation using C#
+
+
+### <b> BY:</b>  Mohammed Rashed Albalawi.
+
+
+<br />
+<hr />
+<br />
--- StrategyPractice/Program.cs
+++ StrategyPractice/Program.cs
@@ -2,11 +2,83 @@
 
 namespace StrategyPractice
 {
+
+    public interface IAttack
+    {
+        public void attack();
+
+    }
+
+    //strategy 1
+    public class ShortRangeAttack : IAttack
+    {
+        public void attack()
+        {
+            Console.WriteLine("punch.....");
+        }
+    }
+
+    //strategy 2
+    public class MidRangeAttack : IAttack
+    {
+        public void attack()
+        {
+            Console.WriteLine("Boom!!!!!!!");
+        }
+    }
+
+    //strategy 3
+    public class LongRangeAttack : IAttack
+    {
+        public void attack()
+        {
+            Console.WriteLine("LAZER! PEW PEW ----------------");
+        }
+    }
+
+
+    public class Player
+    {
+        public string name;
+        public IAttack attackBehaviour;
+
+        public Player(string name ,IAttack attack)
+        {
+            this.name = name;
+            this.attackBehaviour = attack;
+        }
+
+        public void executeAttack()
+        {
+            this.attackBehaviour.attack();
+        }
+
+        public void SetAttack(IAttack attack)
+        {
+            this.attackBehaviour = attack;
+        }
+
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new Player("Mohammed", new ShortRangeAttack());
+            player.executeAttack();
+
+            Console.WriteLine("Enemy GOT AWAY!");
+            player.SetAttack(new LongRangeAttack());
+            player.executeAttack();
+            player.executeAttack();
+
+            Console.WriteLine("Enemy is getting close!");
+            player.SetAttack(new MidRangeAttack());
+            player.executeAttack();
+
+
         }
     }
 }
