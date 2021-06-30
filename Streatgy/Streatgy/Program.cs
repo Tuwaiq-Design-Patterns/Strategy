efc68125edd962ee73a36fb2b1e305810bf283c4
Adel Kalu <adel.b.kalu@gmail.com>
2021-06-30 14:45:37 +0300

    Strategy

---
+++ Streatgy/Streatgy/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    class Strategy
+    {
+        private IStrategy _strategy;
+
+        public Strategy()
+        { }
+
+        public Strategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+
+        public void DoSomeBusinessLogic()
+        {
+            var result = this._strategy.DoAlgorithm(new List<string> { "Adel", "Turki", "Ahmed", "Youns", "Abdullah" });
+
+            string resultStr = null;
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + " ";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+
+    public interface IStrategy
+    {
+        object DoAlgorithm(object data);
+    }
+
+    class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+    }
+
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new Strategy();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+    }
+}
