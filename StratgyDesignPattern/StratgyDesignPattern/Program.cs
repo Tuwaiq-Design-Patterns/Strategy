f7f7f1296b3ce9f94a2156785f3a9404f83f71c4
shahad <shahad@test>
2021-06-30 11:05:54 +0300

    first commit

---
+++ StratgyDesignPattern/StratgyDesignPattern/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace StratgyDesignPattern
+{
+
+    public interface Strategy
+    {
+        public int doOperation(int num1, int num2);
+    }
+    public class OperationAdd : Strategy
+    {
+
+        public int doOperation(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public class OperationSubstract : Strategy
+        {
+
+            public int doOperation(int num1, int num2)
+            {
+                return num1 - num2;
+            }
+        }
+        public class OperationMultiply : Strategy
+        {
+    
+   public int doOperation(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+        public class OperationDivision : Strategy
+        {
+
+            public int doOperation(int num1, int num2)
+            {
+                return num1 / num2;
+            }
+        }
+        public class Context
+        {
+            private Strategy strategy;
+
+            public Context(Strategy strategy)
+            {
+                this.strategy = strategy;
+            }
+
+            public int executeStrategy(int num1, int num2)
+            {
+                return strategy.doOperation(num1, num2);
+            }
+        }
+        public class program
+        {
+           static void Main(string[] args)
+            {
+                Context context = new Context(new OperationAdd());
+                Console.WriteLine("10 + 5 = " + context.executeStrategy(10, 5));
+
+                context = new Context(new OperationSubstract());
+                Console.WriteLine("10 - 5 = " + context.executeStrategy(10, 5));
+
+                context = new Context(new OperationMultiply());
+                Console.WriteLine("10 * 5 = " + context.executeStrategy(10, 5));
+                context = new Context(new OperationDivision());
+
+                Console.WriteLine("10 / 5 = " + context.executeStrategy(10, 5));
+            }
+        }
+          }
+        
+    }
+
