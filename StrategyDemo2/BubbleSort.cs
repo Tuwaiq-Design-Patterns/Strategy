c02278ccd0d656600b66f5b6fe179a792cf8b494
ibra <dev@test.com>
2021-06-30 15:00:36 +0300

    Uploading the HW

---
+++ StrategyDemo2/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using StrategyDemo;
+
+namespace StrategyDemo2
+{
+    public class BubbleSort : ISortStrategy
+    {
+        public void Sort(List<int> list)
+        {
+            Console.WriteLine("BubbleSort list ");
+            int temp;
+            for (int p = 0; p <= list.Count - 2; p++)
+            {
+                for (int i = 0; i <= list.Count - 2; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        temp = list[i + 1];
+                        list[i + 1] = list[i];
+                        list[i] = temp;
+                    }
+                } 
+            }
+        }
+    }
+}
---
+++ StrategyDemo2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using StrategyDemo;
+
+namespace StrategyDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedList sortedList = new SortedList();
+
+            sortedList.SetSortStrategy(new ShellSort());
+            
+            sortedList.Add(4);
+            sortedList.Add(3);
+            sortedList.Add(1);
+            sortedList.Add(9);
+            sortedList.Add(7);
+            sortedList.Add(5);
+            
+            sortedList.Sort();
+        }
+    }
+}
