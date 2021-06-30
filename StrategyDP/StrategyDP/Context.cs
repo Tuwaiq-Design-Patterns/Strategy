3deb458e7379ad801de62253113d61717f98d8b7
HassanAlgadah <h--san@hotmail.com>
2021-06-30 11:00:52 +0300

    final

---
+++ StrategyDP/StrategyDP/Context.cs
@@ -0,0 +1,12 @@
+﻿namespace StrategyDP
+{
+    public class Context
+    {
+        public IStrategy Strategy { get; set; }
+
+        public void executeStrategy()
+        {
+            Strategy.execute();
+        }
+    }
+}
