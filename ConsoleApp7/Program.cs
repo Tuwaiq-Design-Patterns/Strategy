950f0e195562ff25086c5627a5bf2ea6a5be1e0d
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-06-30 20:34:28 +0300

    ANas

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ConsoleApp7.Strategy;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrategyContext context = new StrategyContext(100);
+
+            Console.WriteLine("Enter month number between 1 and 12");
+
+            var input = Console.ReadLine();
+            int month = Convert.ToInt32(input);
+
+            Console.WriteLine("Month =" + month);
+
+            IOffer strategy = context.GetStrategy(month);
+            context.ApplyStrategy(strategy);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp7/Strategy/QuarterDiscount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.Strategy
+{
+    public class QuarterDiscount : IOffer
+    {
+        public string Name => nameof(QuarterDiscount);
+
+        public double GetDiscountPercentage()
+        {
+            return 0.25;
+        }
+    }
+}
