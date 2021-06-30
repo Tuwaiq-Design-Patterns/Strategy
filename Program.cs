91793c6ce43c48f153374d4d4777d8ddc253b66e
AbdulrahmanS <Abdulrhman.Sarawiq@tuwaiq.edu.sa>
2021-06-30 18:44:56 +0300

    WeopenMaker

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Context
+    {
+        private IStrategy _strategy;
+
+        public Context() { }
+
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void WeopenBuilder()
+        {
+            Console.WriteLine("Black smith You're weopen");
+            this._strategy.WeopenBuilder();
+        }
+    }
+
+    public interface IStrategy
+    {
+        void WeopenBuilder();
+    }
+
+    class SwordMaker : IStrategy
+    {
+        public void WeopenBuilder()
+        {
+			Console.WriteLine("Here's your Sword");
+            
+            Console.WriteLine("\t\t\t   *\t\t\t");
+            Console.WriteLine("\t\t\t* *** *\t\t\t");
+            Console.WriteLine("\t\t\t* *** *\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t\t *****\t\t\t");
+            Console.WriteLine("\t\t      ***********\t\t");
+            Console.WriteLine("\t\t\t******\t\t\t");
+            Console.WriteLine("\t\t\t******\t\t\t");
+            Console.WriteLine("\t\t\t******\t\t\t");
+            Console.WriteLine("\t\t\t******\t\t\t");
+        }
+    }
+
+    class DaggersMaker : IStrategy
+    {
+        public void WeopenBuilder()
+        {
+            Console.WriteLine("Here's your Daggers");
+            Console.WriteLine("\t\t\t *\t\t\t \t\t\t *\t\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            Console.WriteLine("\t\t      ********\t\t \t\t\t      ********\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            Console.WriteLine("\t\t\t***\t\t\t \t\t\t***\t\t\t");
+            
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+            Console.WriteLine();
+
+            context.SetStrategy(new SwordMaker());
+            context.WeopenBuilder();
+
+            Console.WriteLine();
+
+            context.SetStrategy(new DaggersMaker());
+            context.WeopenBuilder();
+        }
+    }
+}
